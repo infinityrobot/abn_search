e238e19b8cc2e0d59268778476f8b733a84e3cc0
Ritchie Blair <ritchie@infinityrobot.com>
2015-12-09 18:43:34 +1100

    :fire: Remove unnecessary comments

--- lib/abn_search/abn.rb
+++ lib/abn_search/abn.rb
@@ -30,9 +30,7 @@ module ABNSearch
     #
     # @return [self]
     def update_from_abr!
-      # local cache
       abr_detail = ABNSearch::Client.search(@abn)
-      # parse this stuff
       process_raw_abr_detail(abr_detail)
       self
     end
@@ -41,9 +39,7 @@ module ABNSearch
     #
     # @return [self]
     def update_from_abr_using_acn!
-      # local cache
       abr_detail = ABNSearch::Client.search_by_acn(@acn)
-      # parse this stuff
       process_raw_abr_detail(abr_detail)
       self
     end
